c8738d019e5110b17add6b4d28eaa5a65fb1734e
Sahilkhilar2004 <sahilkhilar2004@gmail.com>
2025-05-24 12:15:42 +0530

    fix: allow Vercel frontend in CORS

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -8,9 +8,18 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
+
+const allowedOrigins = ["https://to-do-list-cyan-rho.vercel.app"];
+
 app.use(
   cors({
-    origin: "to-do-list-cyan-rho.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
